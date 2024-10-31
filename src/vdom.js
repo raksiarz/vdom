31027c6e8bf2ba0edfbb127fb9d730f5c59adf65
raksiarz <raksiarz@gmail.com>
2024-10-31 20:56:24 +0100

    rendering

--- src/vdom.js
+++ src/vdom.js
@@ -1,3 +1,27 @@
 /** @jsx h */
 
 let foo = <div id="foo">Hello</div>
+
+function h(nodeNames, attr, ...args) {
+    let children = args.length ? [].concat(...args) : null
+    return { nodeNames, attr, children}
+}
+
+function render(vnode) {
+    if(vnode.split) {
+        return document.createTextNode(vnode)
+    }
+
+    let n = document.createElement(vnode.nodeName)
+
+    let a = vnode.attributes || {}
+    Object.keys(a).forEach(k => n.setAttribute(k, a[k]))
+
+    (vnode.children || []).forEach(c => n.appendChild(render(c)))
+
+    return n
+}
+
+let dom = render(foo)
+
+document.body.append(dom)
