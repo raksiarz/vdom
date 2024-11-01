84019cf4d2266efde840d47942bc6e2351e04f9a
raksiarz <raksiarz@gmail.com>
2024-11-01 17:55:28 +0100

    mapping list of elements

--- src/vdom.js
+++ src/vdom.js
@@ -1,27 +1,44 @@
 /** @jsx h */
 
-let foo = <div id="foo">Hello</div>
+const number = 6
 
-function h(nodeNames, attr, ...args) {
+let num = <div id="foo">{ number }</div>
+let str = <div><div id="foo">Hello</div></div>
+const items = ['foo', 'bar', 'baz']
+
+function item(text) {
+    return <li>{ text }</li>
+}
+
+let list = render(
+    <ul>
+        { items.map(item) } 
+    </ul>
+)
+
+function h(nodeName, attr, ...args) {
     let children = args.length ? [].concat(...args) : null
-    return { nodeNames, attr, children}
+    return { nodeName, attr, children}
 }
 
 function render(vnode) {
-    if(vnode.split) {
+    if(vnode.split || typeof vnode === 'number') {
         return document.createTextNode(vnode)
     }
 
     let n = document.createElement(vnode.nodeName)
 
-    let a = vnode.attributes || {}
-    Object.keys(a).forEach(k => n.setAttribute(k, a[k]))
+    let a = vnode.attr || {}
+    Object.keys(a).forEach(k => n.setAttribute(k, a[k]));
 
     (vnode.children || []).forEach(c => n.appendChild(render(c)))
 
     return n
 }
 
-let dom = render(foo)
+let dom = render(str)
+let domnum = render(num)
 
 document.body.append(dom)
+document.body.append(domnum)
+document.body.append(list)
